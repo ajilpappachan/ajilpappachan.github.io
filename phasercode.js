f9e4187c91531dc7c75f45995e8d943aed5f4e55
ajilpappachan <ajilpappachan@gmail.com>
2020-06-18 22:24:31 +0530

    Redesigned Layout, Changed Colors and Added Light and Dark Stylesheets

---
+++ phasercode.js
@@ -0,0 +1,29 @@
+var config = 
+{
+    type: Phaser.CANVAS,
+    canvas: document.querySelector('canvas'),
+    backgroundColor: 0xDAEBEB,
+    scene:
+    {
+        preload,
+        create,
+        update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload()
+{
+
+}
+
+function create()
+{
+
+}
+
+function update()
+{
+
+}
